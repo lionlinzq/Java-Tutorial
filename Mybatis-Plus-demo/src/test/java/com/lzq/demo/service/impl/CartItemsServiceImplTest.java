3241f5d1ece7b69227c13ae869574b5ee2a86acb
lionlinzq <niodindeco@hotmail.com>
2023-04-24 18:19:46 +0800

    添加一些文件

--- Mybatis-Plus-demo/src/test/java/com/lzq/demo/service/impl/CartItemsServiceImplTest.java
+++ Mybatis-Plus-demo/src/test/java/com/lzq/demo/service/impl/CartItemsServiceImplTest.java
@@ -1,19 +1,56 @@
 package com.lzq.demo.service.impl;
 
+import com.lzq.demo.domain.CartItems;
 import com.lzq.demo.service.CartItemsService;
+import com.lzq.demo.utils.TestUtil;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 class CartItemsServiceImplTest {
 
-    @Autowired
+    @Mock
     private CartItemsService cartItemsService;
 
+    @InjectMocks
+    private CartItemsServiceImpl cartItemsServiceImpl;
+
+    @Mock
+    private CartItems cartItems;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void test(){
-        cartItemsService.list();
+        // 模拟 Product 对象
+        when(cartItems.getPrice()).thenReturn(BigDecimal.valueOf(10.00));
+    }
+
+    @Test
+    public void testInsertCartItems(){
+        cartItemsService.save(cartItems);
+        System.out.println(cartItemsService.list());
+    }
+
+    @Test
+    public void testGetCartItems(){
+        CartItems items = TestUtil.generate(CartItems.class);
+        cartItemsService.save(items);
+        System.out.println(cartItemsService.list());
     }
 }
